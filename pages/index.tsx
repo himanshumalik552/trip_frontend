f8741cef2ee68973e755a129df15e2b20d9b7dde
Himanshu Malik <himanshumalik552@gmail.com>
2024-06-12 15:45:01 +0530

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,39 @@
+import { Link, button as buttonStyles } from "@nextui-org/react";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import DefaultLayout from "@/layouts/default";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <h1 className={title()}>Make&nbsp;</h1>
+          <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+          <br />
+          <h1 className={title()}>
+            websites regardless of your design experience.
+          </h1>
+          <h4 className={subtitle({ class: "mt-4" })}>
+            Beautiful, fast and modern React UI library.
+          </h4>
+        </div>
+
+        <div className="flex gap-3">
+          <Link
+            isExternal
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href={siteConfig.links.contactus}
+          >
+           Contact Us
+          </Link>    
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+}
