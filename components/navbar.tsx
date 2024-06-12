42a16928ad5996a21f52a17515e204a24b799143
Himanshu Malik <himanshumalik552@gmail.com>
2024-06-12 18:05:49 +0530

    login and registar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -104,6 +104,26 @@ export const Navbar = () => {
             Sponsor
           </Button>
         </NavbarItem> */}
+        <NavbarItem>
+          <NextLink
+            className={clsx(
+              linkStyles({ color: "foreground" }),
+              "data-[active=true]:text-primary data-[active=true]:font-medium",
+            )}
+            color="foreground"
+            href={"/auth/register"}
+          >Register</NextLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NextLink
+            className={clsx(
+              linkStyles({ color: "foreground" }),
+              "data-[active=true]:text-primary data-[active=true]:font-medium",
+            )}
+            color="foreground"
+            href={"/auth/login"}
+          >Login</NextLink>
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
@@ -113,7 +133,6 @@ export const Navbar = () => {
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
-
       <NavbarMenu>
         {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
